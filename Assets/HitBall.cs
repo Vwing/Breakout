9441eae06dea51a6ea925513158e238a2e625eb1
Vwing <vwing@uci.edu>
2015-10-20 18:10:19 -0700

    Scripted the paddle to allow for aiming
    Works pretty well, but the ball jitters a little. Might have to do some
    work in FixedUpdate and replace the Unity physics with our own so it
    feels better.

---
+++ Assets/HitBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBall : MonoBehaviour
+{
+    public float angleMagnitude = 10f;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Ball")
+            return;
+        //Find average contact point
+        Vector3 middleContactPoint = Vector3.zero;
+        foreach (ContactPoint contact in other.contacts)
+        {
+            middleContactPoint += contact.point;
+        }
+        middleContactPoint /= other.contacts.Length;
+
+        middleContactPoint = transform.InverseTransformPoint(middleContactPoint); //converts to local space
+        Vector3 ballDirection = middleContactPoint + Vector3.forward * angleMagnitude; //set direction to hit the ball
+        ballDirection = Vector3.Normalize(ballDirection); //normalize it
+        other.rigidbody.velocity = transform.TransformDirection(ballDirection) * other.rigidbody.velocity.magnitude; //set ball velocity to new direction, same speed.
+    }
+}
