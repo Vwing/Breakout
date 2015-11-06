92ce7412e6483254edffa37ec5decfa2cab1f8ce
danguy123 <danielc0327@yahoo.com>
2015-11-05 18:11:40 -0800

    Started making laser
    It shoots out at an interval

---
+++ Assets/Scripts/LaserBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBehavior : MonoBehaviour {
+    public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "brick")
+            Destroy(other.gameObject);
+
+    }
+}
---
+++ Assets/Scripts/laserGun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserGun : MonoBehaviour {
+    public GameObject laserBeam;
+    public float shootSpeed = 0.5f;
+    private float timer;
+	// Use this for initialization
+	void Start () {
+        timer = shootSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            GameObject.Instantiate(laserBeam, transform.position, transform.rotation);
+            timer = shootSpeed;
+        }
+	}
+}
