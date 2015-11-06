7811bfd73a4b3a07a37992a41e0cb2b40d758866
Shawn Ratcliff <shawn@Montreal.local>
2015-11-06 04:26:03 -0800

    Added whoosh! sound on paddle box hit; added flashing red animation

--- Assets/Scripts/PaddleBox.cs
+++ Assets/Scripts/PaddleBox.cs
@@ -5,9 +5,15 @@ public class PaddleBox : UnityEngine.MonoBehaviour
 {
     public Material PaddleBoxHit;
     public Material paddleBoxReg;
+	public GameObject south;
+	public GameObject east;
+	public GameObject west;
+	public GameObject up;
+	public GameObject down;
 
 	void Start () 
 	{
+
 	}
 
 	void Update ()
@@ -24,17 +30,67 @@ public class PaddleBox : UnityEngine.MonoBehaviour
         }
     }
 
-    IEnumerator Damaged(Collider ball)
-    {
+    IEnumerator Damaged(Collider ball){
         GameManager.lives -= 1;
 
+		// Play whoosh! sound
+		GetComponent<AudioSource> ().Play ();
 
-        float t = 0f;
-        while(t < 0.3f)
-        {
-            t += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-        //mesh.material = paddleBoxReg;
-    }
+		// Flash alert material on the paddle box
+
+		float starttime = Time.time;
+		Renderer r = GetComponent<Renderer> ();
+
+		setAllMaterials(PaddleBoxHit);
+
+		while (Time.time < starttime + 0.1f) {
+			yield return null;
+		}
+		
+		setAllMaterials(paddleBoxReg);
+		starttime = Time.time;
+		
+
+		while (Time.time < starttime + 0.1f) {
+			yield return null;
+		}
+
+		setAllMaterials(PaddleBoxHit);
+
+		while (Time.time < starttime + 0.1f) {
+			yield return null;
+		}
+		
+		setAllMaterials(paddleBoxReg);
+		starttime = Time.time;
+		
+
+		while (Time.time < starttime + 0.1f) {
+			yield return null;
+		}
+		
+		setAllMaterials(PaddleBoxHit);
+		starttime = Time.time;
+		
+		while (Time.time < starttime + 0.2f) {
+			yield return null;
+		}
+		
+		// Return to regular material, unless game lost
+		if (GameManager.lives > 0) {
+			setAllMaterials(paddleBoxReg);
+		}
+	}
+
+	// Sets the material on all walls of the paddle box
+	void setAllMaterials(Material m) {
+		this.GetComponent<Renderer> ().material = m;
+		south.GetComponent<Renderer> ().material = m;
+		east.GetComponent<Renderer> ().material = m;
+		west.GetComponent<Renderer> ().material = m;
+		up.GetComponent<Renderer> ().material = m;
+		down.GetComponent<Renderer> ().material = m;
+	}
+
+
 }
