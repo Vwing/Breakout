e8554d91fcc8f0b5ea6f4189d5c6550bc315c834
Shawn <shawn@Bavaria.local>
2015-11-09 03:31:31 -0800

    Started lab level. Resized paddlebox, paddle and ball to comport with real-world scale for VR.

---
+++ Assets/CeilingLightScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CeilingLightScript : MonoBehaviour {
+
+	private bool enabled;
+	private Color normalEmissionColor;
+	private Renderer r;
+
+	// Use this for initialization
+	void Start () {
+		enabled = true;
+		r = GetComponent<Renderer> ();
+		normalEmissionColor = r.material.GetColor ("_EmissionColor"); // Fetch the normal emission color from the selected material
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enabled) {
+			float emission = 0.1f + 0.8f * Mathf.PerlinNoise (Time.time * 0.5f, 0f); // Range between .1 - .9
+			Color emissionColor = normalEmissionColor * Mathf.LinearToGammaSpace (emission);
+			r.material.SetColor ("_EmissionColor", emissionColor);
+		}
+	}
+
+	void onCollisionEnter(Collision other) {
+		if (other.gameObject.name.Equals ("Ball")) {
+			Color emissionColor = Color.black * Mathf.LinearToGammaSpace (0f);
+			GetComponent<Renderer>().material.SetColor("_EmissionColor", emissionColor);
+			enabled = false;
+		}
+	}
+}
---
+++ Assets/FlickeringLightSource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLightSource : MonoBehaviour {
+
+	private Light l;
+	private float normalRange;
+
+	// Use this for initialization
+	void Start () {
+		l = GetComponent<Light> ();
+		normalRange = l.range;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float multiplier = 0.8f + 0.3f * Mathf.PerlinNoise (Time.time, 0f); // Range between .8 - 1.1
+		l.range = normalRange * multiplier;
+	}
+}
