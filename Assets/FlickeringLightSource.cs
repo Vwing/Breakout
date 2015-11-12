f4e7475ef5b0026dbe61d87d16a26d9ab4285acd
Shawn <shawn@Bavaria.local>
2015-11-11 19:12:53 -0800

    Adding new lab architecture and assets

--- Assets/FlickeringLightSource.cs
+++ Assets/FlickeringLightSource.cs
@@ -4,17 +4,17 @@ using System.Collections;
 public class FlickeringLightSource : MonoBehaviour {
 
 	private Light l;
-	private float normalRange;
+	private float normalIntensity;
 
 	// Use this for initialization
 	void Start () {
 		l = GetComponent<Light> ();
-		normalRange = l.range;
+		normalIntensity = l.intensity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float multiplier = 0.5f + 0.5f * Mathf.PerlinNoise (Time.time, 0f); // Range between .8 - 1.1
-		l.range = normalRange * multiplier;
+		float multiplier = 0.8f + Mathf.PingPong (Time.time, 1.0f);
+		l.intensity = normalIntensity * multiplier;
 	}
 }
