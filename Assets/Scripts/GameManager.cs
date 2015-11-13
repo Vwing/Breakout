f6f8ba15f12988bdd4eb52d106f3717690efd361
Vwing <vwing@uci.edu>
2015-11-13 15:44:27 -0800

    GameManager now automatically counts bricks in the level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,6 @@ using System.Collections;
 public class GameManager : UnityEngine.MonoBehaviour
 {
     [Range (1,7)] public int MaxLives = 3;
-    public int MaxBricks = 20;
-    public int haveBricks;
     public static int lives = 1;
     public static int bricks = 1;
     public static GameManager instance = null;
@@ -22,7 +20,7 @@ public class GameManager : UnityEngine.MonoBehaviour
 	void Awake () 
 	{
         lives = MaxLives;
-        bricks = MaxBricks;
+        bricks = GameObject.FindGameObjectsWithTag("Brick").Length;
         if (instance == null)
             instance = this;
         else if (instance != null)
@@ -46,8 +44,7 @@ public class GameManager : UnityEngine.MonoBehaviour
 
 	void Update ()
 	{
-
-		haveBricks = bricks;
+        haveBricks = bricks;
         if (gameOver)
         {
             if (Input.GetButtonDown("Fire1") || Cardboard.SDK.Triggered)
