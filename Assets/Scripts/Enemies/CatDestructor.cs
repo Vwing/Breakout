cae6930a0f013e8d34a8d70ebe54b85df9900f8e
Isaiah Yu <isaiahy@uci.edu>
2015-11-19 21:07:08 -0800

    Cats die on hitting the "ground"
    -New script CatDestructor attached to ZombieCatProxy GameObjects

---
+++ Assets/Scripts/Enemies/CatDestructor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatDestructor : MonoBehaviour {
+
+    public GameObject SpawnUponDestruction;
+
+    // Use this for initialization
+
+	void Start () {
+	}
+	/*
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    */
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Down")
+        {
+            GameObject.Instantiate(SpawnUponDestruction, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
