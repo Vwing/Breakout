e4ca5fd82e85fb0641ab3e61e5dc90b2f9a8a21a
Shawn <shawnratcliff@users.noreply.github.com>
2015-11-30 00:19:14 -0800

    Added explosion hazard to lab scene

--- Assets/FlickeringLightSource.cs
+++ Assets/FlickeringLightSource.cs
@@ -5,16 +5,24 @@ public class FlickeringLightSource : MonoBehaviour {
 
 	private Light l;
 	private float normalIntensity;
+	private bool enabled;
 
 	// Use this for initialization
 	void Start () {
 		l = GetComponent<Light> ();
 		normalIntensity = l.intensity;
+		enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float multiplier = 0.8f + Mathf.PingPong (Time.time, 1.0f);
-		l.intensity = normalIntensity * multiplier;
+		if (enabled) {
+			float multiplier = 0.8f + Mathf.PingPong (Time.time, 1.0f);
+			l.intensity = normalIntensity * multiplier;
+		}
+	}
+
+	public void setEnabled(bool e) {
+		enabled = e;
 	}
 }
---
+++ Assets/NuclearPool.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NuclearPool : MonoBehaviour {
+
+	public Light nuclearLight;
+	private bool meltedDown;
+
+	void Start() {
+		meltedDown = false;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!meltedDown && other.tag == "Ball") {
+			// Reduce lives to zero
+			GameManager.lives = 0;
+
+			// Stop the ball
+			other.attachedRigidbody.velocity = new Vector3(0,0,0);
+
+			// Start meltdown
+			StartCoroutine("meltdown");
+		}
+	}
+	
+	// Play meltdown lighting sequence
+	IEnumerator meltdown() {
+
+		// Play meltdown sound
+		GetComponent<AudioSource> ().Play ();
+
+		// Suspend existing light animation
+		if (nuclearLight.GetComponent<FlickeringLightSource> ()) {
+			nuclearLight.GetComponent<FlickeringLightSource> ().setEnabled (false);
+		}
+
+		// Blow out the rooom with overpowering light source
+		float startRange = nuclearLight.range;
+		float startIntensity = nuclearLight.intensity;
+		float t0 = Time.time;
+		while (Time.time - t0 < 2f) {
+			nuclearLight.range = Mathf.Lerp(startRange, 100f, (Time.time - t0) / 2);
+			nuclearLight.intensity = Mathf.Lerp(startIntensity, 8f, Time.time - t0);
+			yield return null;
+		}
+	}
+}
