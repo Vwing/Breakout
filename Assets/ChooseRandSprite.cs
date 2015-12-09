526fbeef1a3e181ab870915b9275f42e51ae1f55
Vwing <vwing@uci.edu>
2015-12-09 11:14:14 -0800

    Added cat sprites to cat bricks
    Will update prefab in next commit

---
+++ Assets/ChooseRandSprite.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseRandSprite : MonoBehaviour {
+
+    void Awake()
+    {
+        int ranIndex = (int)(Random.value*transform.childCount);
+        Transform chosenChild = transform.GetChild(ranIndex);
+        chosenChild.gameObject.SetActive(true);
+    }
+}
